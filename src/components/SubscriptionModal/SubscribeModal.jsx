20f45e78eb37e59c382b84a5a3faa047ed456f41
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-01 21:13:20 +0200

    subscription model modified

--- src/components/SubscriptionModal/SubscribeModal.jsx
+++ src/components/SubscriptionModal/SubscribeModal.jsx
@@ -1,72 +1,80 @@
 import React, { useContext, useState } from "react";
-import { GrClose } from "react-icons/gr";
+import { GrClose, GrCheckmark } from "react-icons/gr";
 import { SubscriptionContext } from "../../contexts/SubscriptionContext";
 import styles from "./SubscribeModal.module.scss";
 import Button from "../Button/Button";
 import mountainPicture from "./assets/mountain.jpg";
 import { postSubscription } from "../../consts/postSubscription";
+import { useEffect } from "react";
 
 const SubscribeModal = () => {
+  const [subscribed, setSubscribed] = useState(false);
+  const [emailList, setEmailList] = useState([]);
   const { showSubscription, toggleSubscription } =
     useContext(SubscriptionContext);
 
   const [email, setEmail] = useState("");
 
-  console.log(email);
-
   const content = { email: email };
-
-  console.log(content);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("http://localhost:5000/subscriptions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(content),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (emailList.find((email) => email === content.email)) {
+      alert("You already subscribed");
+    } else {
+      postSubscription(content);
+      setSubscribed(true);
+    }
   };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/subscriptions")
+      .then((response) => response.json())
+      .then((resp) => setEmailList(resp.map((rsp) => rsp.email)));
+  }, [subscribed]);
+
+  const handleClose = () => {
+    toggleSubscription();
+    setSubscribed(false);
+  };
+
+  console.log(emailList);
+  console.log(content.email);
 
   return (
     <aside
       className={showSubscription ? styles["modal--on"] : styles["modal--off"]}
     >
       <section className={styles.modal__block}>
-        <div className={styles.modal__text}>
-          <h1>NEWSLETTER</h1>
-          <h2>Subscribe now</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Insert your email!"
-              required
-            />
-            <Button variant="primary" type="submit">
-              SUBSCRIBE
-            </Button>
-          </form>
-        </div>
+        {subscribed ? (
+          <div className={styles["modal__subscribe-text"]}>
+            <p>Subscribed! Get ready for the best trips of your life!</p>
+            <GrCheckmark />
+          </div>
+        ) : (
+          <div className={styles.modal__text}>
+            <h1 className={styles.modal__heading}>NEWSLETTER</h1>
+            <h2 className={styles.modal__subheading}>Subscribe now</h2>
+            <form className={styles.modal__form} onSubmit={handleSubmit}>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Insert your email!"
+                required
+              />
+              <Button variant="secondary" type="submit">
+                Subscribe
+              </Button>
+            </form>
+            <div className={styles.modal__logo}>Travel</div>
+          </div>
+        )}
         <div className={styles.modal__visual}>
           <img src={mountainPicture} alt="mountain" />
         </div>
         <div className={styles.modal__button}>
-          <Button variant="tertiary" onClick={toggleSubscription}>
+          <Button variant="tertiary" onClick={handleClose}>
             <GrClose />
           </Button>
         </div>
--- src/consts/postSubscription.js
+++ src/consts/postSubscription.js
@@ -1,15 +1,21 @@
 export const postSubscription = (email) => {
-  fetch(`http://localhost:5000/subscriptions`, {
-    method: `POST`,
+  fetch("http://localhost:5000/subscriptions", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      user_email: email,
-    }),
-  }).then((response) => {
-    if (response.ok) {
-      console.log(response.json());
-    }
-  });
+    body: JSON.stringify(email),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };

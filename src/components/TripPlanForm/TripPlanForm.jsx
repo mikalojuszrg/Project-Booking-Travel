ed03249d0b13cbb38053a35513890c8fe9eb2077
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-02 15:46:52 +0200

    date picker fixed

--- src/components/TripPlanForm/TripPlanForm.jsx
+++ src/components/TripPlanForm/TripPlanForm.jsx
@@ -1,21 +1,51 @@
-import { useState } from "react";
-import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import Button from "../Button/Button";
+import "react-calendar/dist/Calendar.css";
 import styles from "./TripPlanForm.module.scss";
 
 const TripPlanForm = () => {
-  const [startDate, setStartDate] = useState(Date.now());
+  const [dates, setDates] = useState([new Date(), new Date()]);
+  const [calendarVisible, setCalendarVisible] = useState(false);
+
   return (
-    <form>
-      <select>plan selection</select>
-      <input type="text" placeholder="Your name" />
-      <input type="email" placeholder="Your email" />
-      <select>number of people</select>
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
-    </form>
+    <>
+      <form className={styles.form}>
+        <input type="text" placeholder="Your name" />
+        <input type="email" placeholder="Your email" />
+        <select>
+          <option>Relaxation</option>
+        </select>
+        <select>
+          <option>Number of people</option>
+        </select>
+        <div>
+          <input
+            type="text"
+            value={dates
+              .map((date) =>
+                date.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                })
+              )
+              .join(" - ")}
+            onClick={() => setCalendarVisible(!calendarVisible)}
+          />
+          {calendarVisible && (
+            <div>
+              <Calendar
+                onChange={(newDates) => setDates(newDates)}
+                selectRange={true}
+                value={dates}
+              />
+            </div>
+          )}
+        </div>
+        <Button variant="primary">Get your offer</Button>
+      </form>
+    </>
   );
 };
 

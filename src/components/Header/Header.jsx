2d616b0c232741d29f8416bea23c33158ef04a0f
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-05 10:18:51 +0200

    responsive navbar added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { BiChevronDown } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { SubscriptionContext } from "../../contexts/SubscriptionContext";
 import Button from "../Button/Button";
@@ -7,41 +8,68 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [showNav, setShowNav] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const { toggleSubscription, showSubscription } =
     useContext(SubscriptionContext);
+
+  const toggleShowNav = () => {
+    setShowNav((prevValue) => !prevValue);
+  };
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if (window.innerWidth > 800) {
+        setShowNav(false);
+      }
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
       <div className={styles.header__logo}>Travel</div>
       <nav>
-        <ul className={styles.header__nav}>
-          <li onClick={() => navigate("/")}>Home</li>
-          <li
-            className={styles.header__dropdown}
-            onMouseEnter={() => setShowDropdown(true)}
-            onMouseLeave={() => setShowDropdown(false)}
+        <AiOutlineMenu
+          className={styles.header__hamburger}
+          onClick={toggleShowNav}
+        />
+        {
+          <ul
+            className={
+              !showNav ? styles.header__nav : styles.header__hamburgerNav
+            }
           >
-            Destinations <BiChevronDown className={styles.header__icon} />
-            {showDropdown && (
-              <ul className={styles.header__subNav}>
-                <li>Bali</li>
-                <li>London</li>
-                <li>Tokyo</li>
-              </ul>
-            )}
-          </li>
-          <li
-            onClick={() => {
-              navigate("/contacts");
-            }}
-          >
-            Contact
-          </li>
-          <Button variant="secondary" onClick={toggleSubscription}>
-            Subscribe
-          </Button>
-        </ul>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li
+              className={styles.header__dropdown}
+              onMouseEnter={() => setShowDropdown(true)}
+              onMouseLeave={() => setShowDropdown(false)}
+            >
+              Destinations <BiChevronDown className={styles.header__icon} />
+              {showDropdown && (
+                <ul className={styles.header__subNav}>
+                  <li>Bali</li>
+                  <li>London</li>
+                  <li>Tokyo</li>
+                </ul>
+              )}
+            </li>
+            <li
+              onClick={() => {
+                navigate("/contacts");
+              }}
+            >
+              Contact
+            </li>
+            <Button variant="secondary" onClick={toggleSubscription}>
+              Subscribe
+            </Button>
+          </ul>
+        }
       </nav>
     </header>
   );

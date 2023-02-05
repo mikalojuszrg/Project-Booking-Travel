794dc963b5fd9070458449d4a0c4c07c456f7f49
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-05 13:25:11 +0200

    plan selection form

--- src/components/TripPlanForm/TripPlanForm.jsx
+++ src/components/TripPlanForm/TripPlanForm.jsx
@@ -3,21 +3,52 @@ import Calendar from "react-calendar";
 import Button from "../Button/Button";
 import "react-calendar/dist/Calendar.css";
 import styles from "./TripPlanForm.module.scss";
+import { plansData } from "../../consts/PlansData";
 
 const TripPlanForm = () => {
   const [dates, setDates] = useState([new Date(), new Date()]);
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [selectedPlan, setSelectedPlan] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState("");
   const [calendarVisible, setCalendarVisible] = useState(false);
+
+  const plansAvailable = plansData.map((plan) => plan.name);
+
+  const userTripinfo = {
+    name: userName,
+    email: userEmail,
+    selectedPlan: selectedPlan,
+    numberOfPeople: numberOfPeople,
+    dates: dates,
+  };
+
+  console.log(userTripinfo);
 
   return (
     <>
       <form className={styles.form}>
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Your email" />
-        <select>
-          <option>Relaxation</option>
+        <input
+          type="text"
+          placeholder="Your name"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Your email"
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+        <select onChange={(e) => setSelectedPlan(e.target.value)}>
+          <option>Select your trip plan</option>
+          {plansAvailable.map((plan) => {
+            return <option>{plan}</option>;
+          })}
         </select>
-        <select>
+        <select onChange={(e) => setNumberOfPeople(e.target.value)}>
           <option>Number of people</option>
+          {Array.from({ length: 10 }, (_, i) => i + 1).map((number) => {
+            return <option>{number}</option>;
+          })}
         </select>
         <div>
           <input

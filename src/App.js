9c94e574228aec54a1a5f359ded27418cd7ae871
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-07 00:02:19 +0200

    cleaned up the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import Routes from "./routes/routes";
 
 const App = () => {
-  // const [backendData, setBackendData] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/api/")
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error(response.statusText);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data.users);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error: " + error);
-  //     });
-  // }, []);
-
   return <Routes />;
 };
 

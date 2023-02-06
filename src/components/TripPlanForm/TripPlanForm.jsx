b966943e79eee4e4a0de3c17f7ade26768d4ef16
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-06 23:50:07 +0200

    form for booking fixed and connected to database

--- src/components/TripPlanForm/TripPlanForm.jsx
+++ src/components/TripPlanForm/TripPlanForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import Button from "../Button/Button";
 import "react-calendar/dist/Calendar.css";
 import styles from "./TripPlanForm.module.scss";
 import { plansData } from "../../consts/PlansData";
+import { postBooking } from "../../utils/postBooking";
 
 const TripPlanForm = () => {
   const [dates, setDates] = useState([new Date(), new Date()]);
@@ -12,6 +13,8 @@ const TripPlanForm = () => {
   const [selectedPlan, setSelectedPlan] = useState("");
   const [numberOfPeople, setNumberOfPeople] = useState("");
   const [calendarVisible, setCalendarVisible] = useState(false);
+  const [bookingList, setBookingList] = useState([]);
+  const [isBooked, setIsBooked] = useState(false);
 
   const plansAvailable = plansData.map((plan) => plan.name);
 
@@ -23,35 +26,59 @@ const TripPlanForm = () => {
     dates: dates,
   };
 
-  console.log(userTripinfo);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (bookingList.find((email) => email === userTripinfo.email)) {
+      alert("You already booked");
+    } else {
+      postBooking(userTripinfo);
+      setUserName("");
+      setUserEmail("");
+      setSelectedPlan("");
+      setNumberOfPeople("");
+      setDates([]);
+      setIsBooked(true);
+    }
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/bookings")
+      .then((response) => response.json())
+      .then((resp) => setBookingList(resp.map((rsp) => rsp.email)));
+  }, []);
 
   return (
     <>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
+          value={userName}
           type="text"
           placeholder="Your name"
           onChange={(e) => setUserName(e.target.value)}
+          required
         />
         <input
+          value={userEmail}
           type="email"
           placeholder="Your email"
           onChange={(e) => setUserEmail(e.target.value)}
+          required
         />
-        <select onChange={(e) => setSelectedPlan(e.target.value)}>
+        <select required onChange={(e) => setSelectedPlan(e.target.value)}>
           <option>Select your trip plan</option>
           {plansAvailable.map((plan) => {
             return <option>{plan}</option>;
           })}
         </select>
-        <select onChange={(e) => setNumberOfPeople(e.target.value)}>
+        <select required onChange={(e) => setNumberOfPeople(e.target.value)}>
           <option>Number of people</option>
           {Array.from({ length: 10 }, (_, i) => i + 1).map((number) => {
             return <option>{number}</option>;
           })}
         </select>
-        <div>
+        <div style={{ position: "relative" }}>
           <input
+            required
             type="text"
             value={dates
               .map((date) =>
@@ -65,7 +92,7 @@ const TripPlanForm = () => {
             onClick={() => setCalendarVisible(!calendarVisible)}
           />
           {calendarVisible && (
-            <div>
+            <div style={{ position: "absolute" }}>
               <Calendar
                 onChange={(newDates) => setDates(newDates)}
                 selectRange={true}
@@ -74,7 +101,14 @@ const TripPlanForm = () => {
             </div>
           )}
         </div>
-        <Button variant="primary">Get your offer</Button>
+
+        {isBooked ? (
+          <p className={styles.form__bookedText}>
+            Thanks for booking! We will get in touch soon.
+          </p>
+        ) : (
+          <Button variant="primary">Get your offer</Button>
+        )}
       </form>
     </>
   );

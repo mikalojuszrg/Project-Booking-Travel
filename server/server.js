33e34019406ed34ee76257bd4b82bdf5018c82b7
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-06 22:45:17 +0200

    updated api

--- server/server.js
+++ server/server.js
@@ -52,6 +52,34 @@ app.post("/subscriptions", async (req, res) => {
   }
 });
 
+app.get("/bookings", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db("travel").collection("bookings").find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/bookings", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db("travel")
+      .collection("bookings")
+      .insertOne(req.body);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({
+      message: "An error occurred while processing your request",
+      error,
+    });
+  }
+});
+
 app.listen(5000, () => {
   console.log("Servers started on port 5000");
 });
